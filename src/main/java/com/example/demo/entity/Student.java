eac175ef10431f6c43041cf63ea70902239a485f
Santosh Shah <santoshofficial679@gmail.com>
2023-10-30 18:09:58 +0545

    Student entity created

---
+++ src/main/java/com/example/demo/entity/Student.java
@@ -0,0 +1,44 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table (
+        name = "tbl_student",
+        uniqueConstraints = @UniqueConstraint(
+                name = "emailId_unique",
+                columnNames = "email_address"
+        )
+)
+public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+    private int studentId;
+    private String firstName;
+    private String lastName;
+
+    @Column(
+            name = "email_address",
+            nullable = false
+    )
+    private String emailId;
+    private String guardianName;
+    private String guardianEmail;
+    private String guardianMobile;
+}
